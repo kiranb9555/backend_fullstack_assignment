94e38eaa28a04b20400de0c2ad47018b3d47b708
kiranb9555 <63695017+kiranb9555@users.noreply.github.com>
2026-06-25 14:24:30 +0530

    df

--- src/redis/redis.ts
+++ src/redis/redis.ts
@@ -1,17 +1,22 @@
-import Redis from "ioredis";
+import IORedis from "ioredis";
 import { env } from "../config/env.js";
 
-declare global {
-  // eslint-disable-next-line no-var
-  var __nexusdialRedis__: Redis | undefined;
-}
-
-export const redis =
-  global.__nexusdialRedis__ ??
-  new Redis(env.redisUrl, {
+const createRedis = () => {
+  return new IORedis(env.redisUrl, {
     maxRetriesPerRequest: null,
     enableReadyCheck: false
   });
+};
+
+type RedisClient = ReturnType<typeof createRedis>;
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __nexusdialRedis__: RedisClient | undefined;
+}
+
+export const redis: RedisClient =
+  global.__nexusdialRedis__ ?? createRedis();
 
 if (process.env.NODE_ENV !== "production") {
   global.__nexusdialRedis__ = redis;
